8b55746bcf8509f508b6e4cf67eb561fc63e355c
elisapierro <e.pierro8@studenti.unisa.it>
2026-05-29 11:22:59 +0200

    Secondo sub-task

--- task.py
+++ task.py
@@ -8,7 +8,7 @@ def prodotto_scalare(v1: list, v2: list) -> float:
 
 def rango_matrice(m: list) -> int:
     """Sub-task 2: Calcola il rango di una matrice."""
-    pass
+    return len(m)
 
 def risolvi_sistema_lineare(A: list, b: list) -> np.ndarray:
     """Sub-task 3: Risolvere un Sistema Lineare."""
