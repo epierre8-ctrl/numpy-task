a71574c3bb836cc8fc2a33eff8ab04dd7813e942
elisapierro <e.pierro8@studenti.unisa.it>
2026-05-29 11:46:48 +0200

    Quarto sub-task

--- task.py
+++ task.py
@@ -16,7 +16,8 @@ def risolvi_sistema_lineare(A: list, b: list) -> np.ndarray:
 
 def correlazione_matrici(m1: list, m2: list) -> np.ndarray:
     """Sub-task 4: Correlazione tra Matrici 2x2."""
-    pass
+    return (np.corrcoef(m1, m2))
+
 
 def operazioni_elemento_per_elemento(v1: list) -> tuple:
     """Sub-task 5: Restituisce (seno, coseno, arcoseno, arcocoseno) elemento per elemento calcolati sul primo array."""
